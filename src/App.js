b03f54ac8507eaefcea3d963be49cb96fc12348f
Dasom Won <59657188+wondasom@users.noreply.github.com>
2020-05-05 16:32:15 +0200

    add nav component

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import "./styles.css";
 import Profile from "./components/Profile";
 import Skills from "./components/Skills";
 import AboutMe from "./components/AboutMe";
+import Nav from "./components/Nav";
 
 export default function App() {
   return (
     <div className="App">
+      <Nav />
       <Profile />
       <AboutMe />
       <Skills />
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -4,31 +4,60 @@ import styles from "./Skills.module.css";
 import ReactLogo from "./ReactLogo";
 // import Emoji from "./Emoji";
 
-export default function Skills() {
-  return (
-    <div className="Skills">
-      <div className={styles.container}>
-        <h1 className={styles.title}>
-          <span className={styles.emoji} role="img" aria-label="tool">
-            🛠
-          </span>
-          Skills
-          <span className={styles.emoji} role="img" aria-label="tool">
-            🛠
-          </span>
-        </h1>
-        <h2 className={styles.subTitle}>Tools I LOVE</h2>
-        {/* <p className={styles.text} /> */}
-        <div className={styles.reactLogo}>
-          <ReactLogo />
+const skillsList = [
+  {
+    skill: "React",
+    level: "✨✨✨",
+    label: "60%"
+  },
+  {
+    skill: "JavaScript",
+    level: "✨✨✨✨",
+    label: "70%"
+  },
+  {
+    skill: "HTML",
+    level: "✨✨✨✨✨",
+    label: "90%"
+  },
+  {
+    skill: "CSS",
+    level: "✨✨✨✨✨",
+    label: "90%"
+  }
+];
+
+class Skills extends React.Component {
+  state = {
+    skills: skillsList
+  };
+  render() {
+    return (
+      <div className="Skills">
+        <div className={styles.container}>
+          <h1 className={styles.title}>
+            <span className={styles.emoji} role="img" aria-label="tool">
+              🛠
+            </span>
+            Skills
+            <span className={styles.emoji} role="img" aria-label="tool">
+              🛠
+            </span>
+          </h1>
+          <h2 className={styles.subTitle}>Tools I LOVE</h2>
+          {/* <p className={styles.text} /> */}
+          <div className={styles.reactLogo}>
+            <ReactLogo />
+          </div>
+          <ul className={styles.tools}>
+            {this.state.skills.map(item => (
+              <li>{item.skill}</li>
+            ))}
+          </ul>
         </div>
-        <ul className={styles.tools}>
-          <li> React</li>
-          <li>JavaScript</li>
-          <li>HTML</li>
-          <li>CSS</li>
-        </ul>
       </div>
-    </div>
-  );
+    );
+  }
 }
+
+export default Skills;
